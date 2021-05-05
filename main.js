d157baa8fcaca0c85dcd6467c094c1ff81ee9b5e
divya212109 <73348305+divya212109@users.noreply.github.com>
2021-05-05 17:06:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+song1 = "";
+song2 = "";
+
+scoreleftWrist = 0;
+scorerightWrist = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+song1_status = "";
+song2_status = "";
+
+function preload() {
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+
+
+
+function setup() {
+
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+    fill("#799c9c");
+    stroke("#739c5d");
+
+
+    if(scorerightWrist > 0.2)
+    {
+        circle(rightWristX, rightWristY, 20); 
+
+        song2.stop();
+
+    if(song1_status == false)
+    {
+        song1.play();
+        document.getElementById("song").innerHTML = "playing the harry potter theme song"
+    }    
+}}
+
+if(scoreleftWrist > 0.2)
+{
+    circle(leftWristX, leftWristY, 20); 
+
+    song1.stop();
+
+    if(song2_status == false)
+    {
+        song2.play();
+        document.getElementById("song").innerHTML = "playing the peter pan song"
+
+    }
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized')
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        scorerightWrist = results[0].pose.keypoints[10].score;
+        console.log("scorerightWrist = " + scorerightWrist + " scoreleftWrist = " + scoreleftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY" + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY" + rightWristY);
+    }
+}
+
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
